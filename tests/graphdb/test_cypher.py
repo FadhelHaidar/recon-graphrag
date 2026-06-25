183ecd0ce4d4e4b94d97d32bc6e37fa94eec6fd1
FadhelHaidar <fadhel1597@gmail.com>
2026-06-25 15:05:55 +0700

    refactor: share backend-neutral utility helpers

--- tests/graphdb/test_cypher.py
+++ tests/graphdb/test_cypher.py
@@ -8,6 +8,10 @@ from recon_graphrag.graphdb.memgraph.cypher import (
 from recon_graphrag.graphdb.neo4j.cypher import (
     escape_cypher_identifier as escape_neo4j_identifier,
 )
+from recon_graphrag.graphdb.cypher import (
+    cypher_string_literal,
+    escape_cypher_identifier,
+)
 
 
 @pytest.mark.parametrize(
@@ -17,3 +21,12 @@ from recon_graphrag.graphdb.neo4j.cypher import (
 def test_escape_cypher_identifier(escape_identifier):
     assert escape_identifier("Movie") == "`Movie`"
     assert escape_identifier("M`ovie") == "`M``ovie`"
+
+
+def test_backend_cypher_helpers_reexport_shared_helpers():
+    assert escape_neo4j_identifier is escape_cypher_identifier
+    assert escape_memgraph_identifier is escape_cypher_identifier
+
+
+def test_cypher_string_literal_escapes_quotes_and_backslashes():
+    assert cypher_string_literal("Bob's \\ Movie") == "'Bob\\'s \\\\ Movie'"
